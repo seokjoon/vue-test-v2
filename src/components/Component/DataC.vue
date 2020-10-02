060d6a211243ff20edacc9570a7490b1601c2bdc
seokjoon <seokjoon@gmail.com>
2020-10-02 10:53:10 +0900

    data

---
+++ src/components/Component/DataC.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>{{ctr}}</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'DataC',
+  created () {
+    //const self = this; setTimeout(function() { self.ctr++; }, 500);
+    this.ctr++;
+  },
+  data: () => ({ ctr: 0, }),
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/Component/DataV.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <DataC></DataC>
+  </div>
+</template>
+
+
+<script>
+import DataC from '@/components/Component/DataC'
+export default {
+  name: "DataV",
+  components: { DataC  },
+  data: function () {
+    return {
+
+    }
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
