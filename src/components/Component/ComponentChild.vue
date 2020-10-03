573c48d7d1dbb64a3121599357dc83ad223bab69
seokjoon <seokjoon@gmail.com>
2020-10-03 17:08:36 +0900

    component

---
+++ src/components/Component/ComponentChild.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>{{ foo }}</div>
+    <ul><li v-for="(v) in bar" v-bind:key="v.k">{{v.k}}</li></ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "ComponentChild",
+  props: {
+    foo: {
+      type: String,
+      default: 'foo',
+      required: true,
+      validator: () => true,
+    },
+    bar: {
+      type: Array,
+    },
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- src/views/Component/ComponentV.vue
+++ src/views/Component/ComponentV.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-
+    <ComponentChild v-bind:foo="foo" v-bind:bar="bar"></ComponentChild>
   </div>
 </template>
 
 
 <script>
+import ComponentChild from '@/components/Component/ComponentChild'
 export default {
-  name: "ComponentV"
+  name: "ComponentV",
+  components: { ComponentChild },
+  data: () => ({
+    foo: 'foo',
+    bar: [ { k: 1 }, { k: 2 } ],
+  }),
 }
 </script>
 
