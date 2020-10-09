474a296227accbb8de84867520380351975f1d21
seokjoon <seokjoon@gmail.com>
2020-10-09 23:34:14 +0900

    slot

---
+++ src/components/Display/SlotScope.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <slot v-bind:foo="foo">
+        {{ foo }}
+      </slot>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "SlotScope",
+  props: {
+    foo: {
+      type: String,
+    },
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- src/views/Display/SlotV.vue
+++ src/views/Display/SlotV.vue
@@ -9,6 +9,8 @@
       <div slot="aa">AA</div>
       <div>BB</div>
     </SlotName>
+    <hr />
+    <SlotScope v-bind:foo="foo"></SlotScope>
   </div>
 </template>
 
@@ -16,12 +18,18 @@
 <script>
 import SlotName from '@/components/Display/SlotName'
 import SlotSingle from '@/components/Display/SlotSingle'
+import SlotScope from '@/components/Display/SlotScope'
 export default {
   name: "SlotV",
   components: {
+    SlotScope,
     SlotSingle,
     SlotName,
   },
+  data: () => ({
+    //items: [ { 'key':1, 'val': 'aa' }, { 'key':2, 'val': 'bb' } ],
+    foo: 'foo',
+  }),
 }
 </script>
 
