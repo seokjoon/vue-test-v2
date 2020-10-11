58f5cbd6922fa34c1aa1adfbfd7ae197793a9925
seokjoon <seokjoon@gmail.com>
2020-10-11 09:13:57 +0900

    store

--- src/store/foo.js
+++ src/store/foo.js
@@ -1,11 +1,18 @@
 const fooStore = {
-  namespace: true,
+  namespaced: true,
   actions: {
-
+    setFooConcatAction(ctx) {
+      ctx.commit('setFooConcat', 'bar');
+    }
+  },
+  getters: {
+    getFooUpper: (state) => state.foo.toUpperCase(),
   },
   modules: {},
   mutations: {
-
+    setFooConcat(state, str) {
+      if(typeof str === "string") state.foo = state.foo.concat(str);
+    }
   },
   state: {
     foo: 'foo',
--- src/views/Data/StoreV.vue
+++ src/views/Data/StoreV.vue
@@ -9,13 +9,24 @@
 export default {
   name: "StoreV",
   created () {
-    console.log(this.$store.state.ctr, this.$store.getters.getCtrSquare);
-    this.$store.commit('setCtrAdd', 2);
-    console.log(this.$store.state.ctr, this.$store.getters.getCtrSquare);
-    this.$store.dispatch('setCtrAddAction');
-    console.log(this.$store.state.ctr, this.$store.getters.getCtrSquare);
+    //this.testStore();
+    this.testStoreFoo();
   },
   methods: {
+    testStore() {
+      console.log(this.$store.state.ctr, this.$store.getters.getCtrSquare);
+      this.$store.commit('setCtrAdd', 2);
+      console.log(this.$store.state.ctr, this.$store.getters.getCtrSquare);
+      this.$store.dispatch('setCtrAddAction');
+      console.log(this.$store.state.ctr, this.$store.getters.getCtrSquare);
+    },
+    testStoreFoo() {
+      console.log(this.$store.state.fooStore.foo, this.$store.getters['fooStore/getFooUpper']);
+      this.$store.commit('fooStore/setFooConcat', 'bar');
+      console.log(this.$store.state.fooStore.foo, this.$store.getters['fooStore/getFooUpper']);
+      this.$store.dispatch('fooStore/setFooConcatAction');
+      console.log(this.$store.state.fooStore.foo, this.$store.getters['fooStore/getFooUpper']);
+    }
   },
 }
 </script>
